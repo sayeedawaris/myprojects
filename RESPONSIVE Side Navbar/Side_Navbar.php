b58231a7d322fa18f2cc546fcf03275c5e61aebb
Sayeeda Waris <106420051+sayeedawaris@users.noreply.github.com>
2024-01-31 14:43:01 +0530

    Add files via upload

---
+++ RESPONSIVE Side Navbar/Side_Navbar.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="styless.css"> 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <nav>
+    <ul>
+    <li><a href="#"><i class="fas fa-solid fa-user "></i><span class="nav-item"> Admin</span></a></li>
+    <li><a href="#"><i class="fas fa-solid fa-list "></i><span class="nav-item"> Employee List</span></a></li>
+    <li><a href="#"><i class="fas fa-solid fa-bookmark "></i><span class="nav-item"> Payroll List</span></a></li>
+    <li><a href="#"><i class="fas fa-solid fa-flag "> </i><span class="nav-item"> Deduction List</span></a></li>
+</ul>
+</nav>
+</div>
+</div>
+</div>
+</body>
+</html>
